e5293ed39bea21f0304e92b3c8860dd879cb7add
mnem0nic7 <m7.ga.77@gmail.com>
2025-05-16 13:16:33 -0700

    1.0.0

---
+++ part_1.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+add1 = num1 + num2
+sub1 = num1 - num2
+
+print("Their sum is:", add1)
+print("Their difference is:", sub1)
---
+++ part_2.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+mult1 = num1 * num2
+div1 = num1 / num2
+
+print("Their product is:", mult1)
+print("Their quotient is:", div1)
